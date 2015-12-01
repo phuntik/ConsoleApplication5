442bc9af39b1055a2195fa8131019a2183ed7c3c
Сергей Самовол <s.samovol@outlook.com>
2015-12-01 23:30:46 +0300

    lal

---
+++ ConsoleApplication5/Source.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+void degree(int m1[3][3], int m2[3][3], int m3[3][3])
+{	
+	for (int i = 0; i < 3; i++) 
+	{
+		for (int j = 0; j < 3; j++) 
+		{
+			m3[i][j] = 0; //матрица результата
+			for (int k = 0; k < 3; k++) 
+			{
+				m3[i][j] += m1[i][k] * m2[k][j];
+			}
+		}
+	}		
+}
+
+void out(int m3[3][3])
+{
+	system("cls");
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << m3[i][j] << ' ';
+		}
+		cout << endl;
+	}
+}
+void main()
+{
+	int M1[3][3], M2[3][3], m1[3][3], m2[3][3], m3[3][3];		/*	m2,m3 - вспомогательные матрицы,		*/
+	cout << "Fill in first matrix" << endl;						/*	используемые при возведении в степень,	*/
+	for (int s = 0;s < 3;s++)//строки							/*	чтобы не трогать искомыу матрицы.		*/
+	{
+		for (int c = 0;c < 3;c++)//столбцы
+		{
+			cin >> M1[s][c];
+		}
+	}
+	cout << "Fill in second matrix" << endl;
+	for (int s = 0;s < 3;s++)//строки
+	{
+		for (int c = 0;c < 3;c++)//столбцы
+		{
+			cin >> M2[s][c];
+		}
+	}
+	int numb = 0, stepen = 0;
+	cout << "Power first matrix - push 1\nMultiply matrics - push 2" << endl;
+	cin >> numb;
+	switch (numb) //выбор задания
+	{
+		case 1: 
+		{
+			memcpy(m2, M1, sizeof(m2));
+			cout << "Degree?" << endl;
+			cin >> stepen;
+			degree(M1, m2, m3);
+			for (int i = 1;i < stepen;i++)
+			{
+				memcpy(m2, m3, sizeof(m2));
+				degree(M1, m2, m3);
+			}
+			out(m3);		
+		}
+			break;
+		case 2:
+		{
+			degree(M1, M2, m3);
+			out(m3);
+		}
+			break;
+	}
+
+
+//fin
+}
